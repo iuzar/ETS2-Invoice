b97df99b4b54c60ca22d0eebf22f50d3fd11fe85
Chakratos <chakratoss@gmail.com>
2017-05-28 02:48:24 +0200

    Initial Commit and first class
    Added class User

---
+++ ETS2-Invoice/Classes/Database/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTest1
+{
+    class User
+    {
+        public int id, fk_creationpassword, is_admin;
+        public string username, password;
+        public DateTime creationdate;
+    }
+}
