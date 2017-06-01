ef29f4f8ae50280f692416652a6212105433994e
Chakratos <chakratoss@gmail.com>
2017-06-01 23:31:21 +0200

    Added Crashhandler and removed obj folder

--- ETS2-Invoice/App.xaml.cs
+++ ETS2-Invoice/App.xaml.cs
@@ -13,5 +13,11 @@ namespace ETS2_Invoice
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Exception Caught", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+            Application.Current.Shutdown();
+        }
     }
 }
